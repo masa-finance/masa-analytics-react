648d8ba6240b6c6a7d8a6e6550e3d871f4fd925a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-07-06 09:37:31 +0200

    fix types

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   // Set the mode to production to enable various optimizations like minification
-  mode: 'production',
+  mode: "production",
   // Entry point for the webpack bundling process
-  entry: './dist/index.js',
+  entry: "./dist/index.js",
   // Output configuration for the bundled file
   output: {
     // The filename of the bundled file
-    filename: 'zksbt-cookie.min.js',
+    filename: "zksbt-cookie.min.js",
     // The path where the bundled file will be saved
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     // The name of the library when used as a script
-    library: 'zksbtCookie',
+    library: "zksbtCookie",
     // The type of module definition to use
-    libraryTarget: 'umd',
+    libraryTarget: "umd",
     // Ensures that the library works with different environments (Node.js, browser)
-    globalObject: 'this',
+    globalObject: "this",
   },
   // Optimization settings for minimizing the output bundle
   optimization: {
@@ -25,15 +25,14 @@ module.exports = {
     minimize: true,
     // Use TerserPlugin to minify the JavaScript code
     minimizer: [new TerserPlugin()],
-  },
-  // Module rules for handling different types of files
+  }, // Module rules for handling different types of files
   module: {
     rules: [
       {
         // Test for TypeScript files
         test: /\.tsx?$/,
         // Use ts-loader to transpile TypeScript files to JavaScript
-        use: 'ts-loader',
+        use: "ts-loader",
         // Exclude the node_modules folder from the bundling process
         exclude: /node_modules/,
       },
@@ -41,6 +40,6 @@ module.exports = {
   },
   // File extensions that webpack will resolve automatically
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
