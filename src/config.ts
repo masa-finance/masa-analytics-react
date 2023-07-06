c462ec648d48f86b10ed493fc90b1c017aeaceef
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-07-06 16:47:52 +0200

    make api url configurable

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const API_URL =
-  process.env.COOKIE_MONSTER_API_URL ?? "https://zksbt-cookie-api.onrender.com";
+  process.env.REACT_APP_COOKIE_MONSTER_API_URL ??
+  "https://zksbt-cookie-api.onrender.com";
