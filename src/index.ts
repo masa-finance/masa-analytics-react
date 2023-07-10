e347c6992d4bb5869dc79ba398da49716b2bedce
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-07-10 15:22:09 +0200

    remove dead code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
-import CookieConsentBanner from "./CookieConsentBanner";
 import { useCookieMonster } from "./hooks/useCookieMonster";
 import { useEventLogger } from "./hooks/useEventLogger";
 import { useUser } from "./hooks/useUser";
-import type {
-  MessageSigningEventData,
-  PageVisitEventData,
-} from "./interfaces/EventData";
-import { signMessageAndTrack } from "./messageSigningTracker";
-import { trackPageVisit } from "./pageVisitTracker";
-import { getCookie, setCookie } from "./utils/cookieUtils";
 
 export {
-  // consent
-  CookieConsentBanner,
-  // cookie handling
-  setCookie,
-  getCookie,
-  // signing
-  signMessageAndTrack,
-  MessageSigningEventData,
-  // tracking
-  trackPageVisit,
-  PageVisitEventData,
   // new cookie monster
   useUser,
   useEventLogger,
--- src/interfaces/EventData.ts
+++ src/interfaces/EventData.ts
@@ -1,19 +1,4 @@
-// Define the structure of the event data for message signing
-export interface MessageSigningEventData {
-  eventType: string;
-  accountId: string;
-  message: string;
-  signature: string;
-}
-
 export type EventTypes = "pageView" | "login" | "mint" | "connectWallet";
-
-// Interface for page visit event data
-export interface PageVisitEventData {
-  eventType: string;
-  accountId: string;
-  url: string;
-}
 
 export interface Event {
   type: EventTypes;
