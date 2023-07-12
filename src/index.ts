42e7029933be44a455621a92560ac3a2d44b7ef9
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-07-12 15:55:55 +0200

    remove dead code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { useCookieMonster } from "./hooks/useCookieMonster";
 import { useEventLogger } from "./hooks/useEventLogger";
-import { useUser } from "./hooks/useUser";
 
 export {
   // new cookie monster
-  useUser,
   useEventLogger,
   useCookieMonster,
 };
