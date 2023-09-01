41d4258a78d66f55648a045d7624dc1c1d7e6360
hide-on-bush-x <hideonbushx123@gmail.com>
2023-09-01 19:13:54 -0300

    Adding flexible event

--- src/hooks/useMasaAnalyticsReact.tsx
+++ src/hooks/useMasaAnalyticsReact.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import {
   ConnectWalletEventData,
+  EventTypes,
   FireMintEventArgs,
   LoginEventData,
   MintEventData,
@@ -170,6 +171,60 @@ export const useMasaAnalyticsReact = ({
       },
       [logEvent, clientName, clientApp, clientId]
     );
+    
+
+
+  /**
+   * Fire a flexible event
+   * It can use any of the EventTypes
+   */
+  const fireEvent = useCallback(
+    async (type: EventTypes, {
+      user_address,
+      token_name,
+      ticker,
+      token_type,
+      network,
+      contract_address,
+      mint_fee,
+      mint_currency,
+      fee_asset,
+      asset_amount,
+      additionalEventData,
+    }: FireMintEventArgs) => {
+      const event_data: MintEventData = {
+        client_app: clientApp,
+        client_name: clientName,
+        token_name,
+        ticker,
+        token_type,
+        network,
+        contract_address,
+        mint_fee,
+        mint_currency,
+        fee_asset,
+        asset_amount,
+      };
+
+      try {
+        await logEvent({
+          type,
+          user_address,
+          client_id: clientId,
+          event_data: {
+            ...event_data,
+            ...additionalEventData,
+          },
+
+          endpoint: 'tracking',
+        });
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('fireEvent():', error);
+      }
+    },
+    [logEvent, clientName, clientApp, clientId]
+  );
 
     return {
       fireLoginEvent,
