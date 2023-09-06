8f4aefa7b25ba01ab1b04e65fb641ce555e36c09
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-09-06 14:27:01 +0200

    added docs

---
+++ typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entryPoints: ['./src/index.ts'],
+  plugin: 'typedoc-plugin-markdown',
+  name: '# Masa Analytics React\n',
+  out: 'docs',
+  disableSources: true,
+  includeVersion: true,
+};
