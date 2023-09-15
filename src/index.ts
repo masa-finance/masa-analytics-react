19fe6e5b7463cc91c10cba7737ea73d9fdaccb65
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-09-15 17:24:06 +0200

    update sdk

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { generateSignature } from '@masa-finance/analytics-sdk';
-
+export {
+  generateSignature,
+  analyticsClient,
+} from '@masa-finance/analytics-sdk';
 export { useEventLogger } from './hooks/useEventLogger';
 export { useMasaAnalyticsReact } from './hooks/useMasaAnalyticsReact';
 export { EventType } from '@masa-finance/analytics-sdk';
-export const utils = {
-  generateSignature,
-};
