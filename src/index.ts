49db7f50b24c3ba8bf5d4e19101b8995658868a2
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-09-21 14:23:15 +0200

    update interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {
   generateSignature,
-  analyticsClient,
+  MasaAnalytics,
+  EventType,
 } from '@masa-finance/analytics-sdk';
 export { useEventLogger } from './hooks/useEventLogger';
 export { useMasaAnalyticsReact } from './hooks/useMasaAnalyticsReact';
-export { EventType } from '@masa-finance/analytics-sdk';
